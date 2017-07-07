cb6b028c47ca35fb1e3bdd1113eaddc465437285
Luis Muñoz <lmunoz@kobsa.com.pe>
2017-07-07 18:04:10 -0500

    Creación de migracion usuarios

--- database/migrations/2017_06_28_161440_create_usuarios_table.php
+++ database/migrations/2017_06_28_161440_create_usuarios_table.php
@@ -15,10 +15,24 @@ class CreateUsuariosTable extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nombre');
-            $table->string('direccion');
-            $table->string('telefono');
-            $table->string('email')->unique();
+            $table->string("dni")->unique();
+            $table->string("ap_paterno");
+            $table->string("ap_materno");
+            $table->string("nombres");
+            $table->date("fecnac");
+            $table->string("est_civil");
+            $table->date("fec_ingreso");
+            $table->string("movil");
+            $table->string("fijo");
+            $table->string("direccion");
+            $table->string("ubigeo");
+            $table->string("email_corp");
+            $table->string("email_per");
+            $table->string("login");
+            $table->string("contacto_emergencia");
+            $table->string("telef_contacto");
+            $table->string("perfil");
+            $table->string("turno");
             $table->rememberToken();
             $table->timestamps();
         });
